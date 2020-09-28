cc2835b3dd01197e84aa1df0af3026d0279bc418
Rafe Nasir <rafenasir@gmail.com>
2020-09-29 00:31:17 +0200

    Created function for value replacement

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let assets = {
     credit: 0
 };
 
-let expenses = {
+var expenses = {
     debit: 0,
     credit: 0
 };
@@ -24,6 +24,7 @@ let equity = {
 function findValue(x) {
     if (document.getElementById("inputGroupSelect01").value === "1") {
         expenses.debit = expenses.debit + x;
+        document.getElementById("exp").innerHTML = expenses.debit - expenses.credit;
         document.getElementById("amount").value = null
         return expenses.debit
 
@@ -31,12 +32,14 @@ function findValue(x) {
     //to decrease the value of expense.
     else if (document.getElementById("inputGroupSelect01").value == "2") {
         expenses.credit = expenses.credit + x;
+        document.getElementById("exp").innerHTML = expenses.debit - expenses.credit;
         document.getElementById("amount").value = null
         return expenses.credit
     }
     // Increase Assets
     else if (document.getElementById("inputGroupSelect01").value == "3") {
         assets.debit = assets.debit + x;
+        document.getElementById("ass").innerHTML = assets.debit - assets.credit;
         document.getElementById("amount").value = null
         return assets.debit
 
@@ -44,6 +47,7 @@ function findValue(x) {
     // decrease asset
     else if (document.getElementById("inputGroupSelect01").value == "4") {
         assets.credit = assets.credit + x;
+        document.getElementById("ass").innerHTML = assets.debit - assets.credit;
         document.getElementById("amount").value = null
         return assets.credit
 
@@ -51,12 +55,14 @@ function findValue(x) {
     // Increase Income
     else if (document.getElementById("inputGroupSelect01").value == "5") {
         incomes.credit = incomes.credit + x;
+        document.getElementById("inc").innerHTML = incomes.credit - incomes.debit;
         document.getElementById("amount").value = null
         return income.credit
     }
     // decrease income
     else if (document.getElementById("inputGroupSelect01").value == "6") {
         incomes.debit = incomes.debit + x;
+        document.getElementById("inc").innerHTML = incomes.credit - incomes.debit;
         document.getElementById("amount").value = null
         return incomes.debit
 
@@ -64,6 +70,7 @@ function findValue(x) {
     // liabilities increased
     else if (document.getElementById("inputGroupSelect01").value == "7") {
         liabilities.credit = liabilities.credit + x;
+        liabilitiesValue();
         document.getElementById("amount").value = null
         return liabilities.credit
 
@@ -71,6 +78,7 @@ function findValue(x) {
     // liabilities decreased
     else if (document.getElementById("inputGroupSelect01").value == "8") {
         liabilities.debit = liabilities.debit + x;
+        liabilitiesValue();
         document.getElementById("amount").value = null
         return liabilities.debit
     }
@@ -78,23 +86,51 @@ function findValue(x) {
     // equity increased
     else if (document.getElementById("inputGroupSelect01").value == "9") {
         equity.credit = equity.credit + x;
+        equityValue();
         document.getElementById("amount").value = null
         return equity.credit
     }
     // equity decreased
     else if (document.getElementById("inputGroupSelect01").value == "10") {
         equity.debit = equity.debit + x;
+        equityValue();
         document.getElementById("amount").value = null
         return equity.debit
     } else {
         alert("Invalid Head")
     }
+    amountTotal();
+
 }
 
-let finalValue = {
-    expenses: expenses.debit - expenses.credit,
+
+function equityValue() {
+    let equity1 = equity.credit - equity.debit;
+    document.getElementById("equity").innerHTML = equity1;
+    return equity1
+
+}
+
+function liabilitiesValue() {
+    let liabilities1 = liabilities.credit - liabilities.debit;
+    document.getElementById("liab").innerHTML = liabilities1;
+    return liabilities1
+}
+
+function amountTotal() {
+    let total1 = liabilitiesValue() + equityValue();
+}
+
+
+
+let y = {
+    expensed: expenses.debit - expenses.credit,
     assets: assets.debit - assets.credit,
     incomes: incomes.credit - incomes.debit,
     liabilities: liabilities.credit - liabilities.debit,
     equity: equity.credit - equity.debit
 }
+
+/* let calcExpenses = {
+    y.expensed = assets.debit - assets.credit
+} */
